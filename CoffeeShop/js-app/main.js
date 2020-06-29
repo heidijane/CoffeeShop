62dafb4cd755113cde7b0597e5bb2a11c6bdaed2
Heidi Sprouse <hsprouse91@gmail.com>
2020-06-29 14:39:45 -0500

    Bean Varieties now load on the DOM when 'Bean Varieties' button is clicked. Added some basic bootstrap styling

--- CoffeeShop/js-app/main.js
+++ CoffeeShop/js-app/main.js
@@ -1,13 +1,34 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
+const varietyContainer = document.querySelector("#varietyContainer");
+
+let beanVarieties = [];
+
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
-        .then(beanVarieties => {
-            console.log(beanVarieties);
+        .then(response => {
+            console.log(response);
+            beanVarieties = response;
+            renderVarieties();
         })
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+const renderVarieties = () => {
+    varietyContainer.innerHTML = "";
+    varietyContainer.innerHTML += beanVarieties.map(variety => {
+        return `
+                <div class="card m-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${variety.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${variety.region}</h6>
+                        <p class="card-text">${variety.notes}</p>
+                    </div>
+                </div>
+        `;
+    }).join('');
 }
